1e3115030ac39410ae29f83cc05862ee9c9dedce
27ner1k <ivanycpetr15@gmail.com>
2026-07-15 15:49:09 +0300

    1 checker PageRank

---
+++ test.py
@@ -0,0 +1,21 @@
+import networkx as nx
+import csv
+import sys
+
+edges = []
+with open(sys.argv[1]) as f:
+    reader = csv.reader(f)
+    next(reader)
+    for row in reader:
+        fromm = int(row[0])
+        to = int(row[1])
+        edges.append((fromm, to))
+
+G = nx.DiGraph()
+G.add_edges_from(edges)
+pr = nx.pagerank(G, alpha=0.85)
+
+with open(sys.argv[2], "w") as f:
+     f.write(f"vertex,rank\n")
+     for i in sorted(pr):
+         f.write(f"{i},{pr[i]}\n")
