28a6b8f864d151bfb7949dc232d053f41cdb4b10
27ner1k <ivanycpetr15@gmail.com>
2026-07-17 20:50:51 +0300

    Add external memory PageRank implementation

--- src/main.cpp
+++ src/main.cpp
@@ -10,39 +10,36 @@ int main(int argc, char* argv[]) {
     std::ifstream f(argv[1]);
     std::string line;
     std::getline(f, line);
-    std::vector<std::pair<int32_t, int32_t>> edges(0);
-    size_t lenght = 0;
-    while (std::getline(f, line)) { 
-        std::stringstream ss(line);
-        std::string from, to;
-        std::getline(ss, from, ',');
-        std::getline(ss, to, ',');
-        edges.push_back(std::pair<int32_t, int32_t>{std::stoi(from), std::stoi(to)});
-        lenght += 1;
-    }
-    f.close();
 
     std::unordered_map<int32_t, int32_t> to_new_id;
     std::vector<int32_t> to_back_id;
+    std::vector<int32_t> degree_vertex;
     int32_t now = 0;
-    for (auto[from, to] : edges){
+    
+    while (std::getline(f, line)) { 
+        std::stringstream ss(line);
+        std::string s_from, s_to;
+        std::getline(ss, s_from, ',');
+        std::getline(ss, s_to, ',');
+        int32_t from = std::stoi(s_from);
+        int32_t to = std::stoi(s_to);
         if (to_new_id.find(from) == to_new_id.end()){
             to_new_id[from] = now;
             to_back_id.push_back(from);
             now += 1;
+            degree_vertex.push_back(0);
         }
         if (to_new_id.find(to) == to_new_id.end()){
             to_new_id[to] = now;
             to_back_id.push_back(to);
             now += 1;
+            degree_vertex.push_back(0);
         }
-    }
-    
-    int32_t to_back_id_size = to_back_id.size();
-    std::vector<int32_t> degree_vertex(to_back_id_size, 0);
-    for (auto[from, to] : edges){
         degree_vertex[to_new_id[from]] += 1;
     }
+    f.close();
+
+    int32_t to_back_id_size = to_back_id.size();
     std::vector<double> vertex_rank(to_back_id_size, double (1 / double(to_back_id_size)));
     std::vector<double> new_vertex_rank(to_back_id_size, double (1 / double(to_back_id_size)));
     constexpr double damping_vertex = 0.85;
@@ -59,11 +56,21 @@ int main(int argc, char* argv[]) {
         for (int i = 0; i < to_back_id_size; ++i)
             new_vertex_rank[i] = base;
 
-        for (auto [from, to] : edges) {
+        std::ifstream f(argv[1]);
+        std::string line;
+        std::getline(f, line);
+        while (std::getline(f, line)) { 
+            std::stringstream ss(line);
+            std::string s_from, s_to;
+            std::getline(ss, s_from, ',');
+            std::getline(ss, s_to, ',');
+            int32_t from = std::stoi(s_from);
+            int32_t to = std::stoi(s_to);
             int new_from = to_new_id[from];
             int new_to = to_new_id[to];
             new_vertex_rank[new_to] += damping_vertex * vertex_rank[new_from] / degree_vertex[new_from];
         }
+        f.close();
         vertex_rank = new_vertex_rank;
     }
 
